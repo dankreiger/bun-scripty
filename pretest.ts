0e6d4768602450d6e7e8a347bdc969a2068bcbc8
Daniel Kreiger <dankreiger@gmail.com>
2024-09-20 01:19:29 +0200

    fix: add postinstall hook

--- pretest.ts
+++ pretest.ts
@@ -5,7 +5,6 @@ import { $ } from 'bun';
 if (!process.env.CI) {
   console.log('Giving tests newest binary...');
   await $`bun run build`;
-  await $`bun link`;
 } else {
   console.log('Running tests in CI...');
 }
